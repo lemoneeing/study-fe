72ebfc8773ac0f615cca489a104e67056f01ce27
Jeonghyun Kim <jh2two@gmail.com>
2025-04-22 13:00:17 +0900

    4강_API로뉴스GET:v-bind, props

--- pages/components/Card.vue
+++ pages/components/Card.vue
@@ -1,22 +1,41 @@
 <script setup lang="ts">
+import type {Article} from "~/types/api.ts";
+import dayjs from "dayjs";
+
+interface Props {
+    data: Article;
+}
+
+const props = defineProps<Props>();
+const { data } = toRefs(props);
+
+const url = data.value.url;
+const website = computed(()=>{
+    return url.split("https://").pop()?.split("/")[0];
+})
+
+const logo = computed(() => {
+    return `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://${website.value}&size=16`;
+})
 </script>
 
 <template>
     <div class="card">
         <div class="card_image-box">
-            <img src="" alt="" />
+            <img v-if="data.urlToImage" :src="data.urlToImage" alt="" class="card_image-box_image" />
+            <div v-else class="card_image-box_noData">등록된 이미지가 없습니다.</div>
         </div>
         <div class="card_info-box">
             <div class="card_info-box_header">
                 <div class="broadcast">
-                    <img src="" alt="" class="broadcast_logo" />
-                    <span class="broadcast_complany">KBS/SBS/MBC</span>
+                    <img :src="logo" alt="" class="broadcast_logo" />
+                    <span class="broadcast_complany">{{data.source.name}}</span>
                 </div> 
-                <span class="title">뉴스제목</span>
+                <span class="title">{{data.title}}</span>
             </div>
             <div class="card_info-box_body">
                 <span class="contents">
-                    뉴스 기사내용 뉴스 기사내용 뉴스 기사내용뉴스 기사내용 뉴스 기사내용 뉴스 기사내용뉴스 기사내용뉴스 기사내용뉴스 기사내용뉴스 기사내용뉴스 기사내용뉴스 기사내용뉴스 기사내용
+                    <span v-html="data.content"></span>
                 </span>
             </div>
         </div>
@@ -25,27 +44,27 @@
                 <img src="@/assets/icons/user_icon.svg" alt="" class="image" />
             </div>
             <div class="card_profile-box_detail">
-                <span class="name">Name</span>
-                <span class="date">2025.04.22</span>
+                <span class="name">{{data.author}}</span>
+                <span class="date">{{dayjs(data.publishedAt).format("YYYY-MM-DD HH:mm:ss")}}</span>
             </div>
         </div>
     </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss" coped>
 @use "../../assets/scss/color";
 @use "../../assets/scss/mixin";
-
-.card{
-    @include mixin.flex-box(flex-start, center);
+.card {
+    @include mixin.flexbox(flex-start, flex-start);
     flex-direction: column;
 
     width: calc(360px - 48px);
-    height: 400px;
+    height: 440px;
 
-    padding: 24px;
-    gap: 24px;
+    padding: 24px !important;
+    gap: 20px;
 
+    // background-color: $color-black-700; // 다크모드
     background-color: color.$color-white-000;
     box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.2);
     border: 0.5px solid color.$color-gray-000;
@@ -53,72 +72,102 @@
 
     cursor: pointer;
 
-    &_image-box{
+    &_image-box {
         width: 100%;
-        height: 192px;
+        min-height: 192px;
 
-        background-color: beige;
         border-radius: 12px;
-    }
 
-    &_info-box{
+        &_image {
+            width: 100%;
+            height: 100%;
+
+            border-radius: 12px;
+        }
+        &_noData {
+            @include mixin.flex-center;
+
+            width: 100%;
+            height: 100%;
+
+            border: 0.5px solid color.$color-gray-300;
+            border-radius: 12px;
+            color: color.$color-gray-350;
+        }
+    }
+    &_info-box {
         @include mixin.flex-center;
         flex-direction: column;
-        flex: 1;
-        
-        width:100%;
-        gap: 12px;
 
-        &_header{
-            @include mixin.flex-box(center, flex-start);
+        width: 100%;
+
+        &_header {
+            @include mixin.flexbox(center, flex-start);
             flex-direction: column;
 
             width: 100%;
+            height: 88px;
 
             gap: 8px;
 
-            &_logo{
-                width: 20px;
-                height: 20px;
-
-                border-radius: 50%;
-                background-color: white;
-            }
-
-            &_company{
-                @include mixin.flex-center;
-                font-size: 12px;
-                color: color.$color-black-700;
-            }
-
             .broadcast {
-                @include mixin.flex-box(center, flex-start);
+                @include mixin.flexbox(center, flex-start);
                 width: 100%;
 
                 gap: 8px;
-            }
 
+                &_logo {
+                    width: 20px;
+                    height: 20px;
+
+                    border-radius: 50%;
+                    background-color: white;
+                }
+                &_company {
+                    @include mixin.flex-center;
+                    font-size: 12px;
+                    color: color.$color-black-700;
+                }
+            }
             .title {
-                @include mixin.flex-box(center, flex-start);
+                @include mixin.flexbox(center, flex-start);
                 width: 100%;
                 height: 48px;
 
                 color: color.$color-black-700;
+
+                overflow: hidden;
+                white-space: normal;
+                text-overflow: ellipsis;
+                display: -webkit-box;
+                -webkit-line-clamp: 2;
+                -webkit-box-orient: vertical;
+                word-break: keep-all; // 문단으로 끊어져서 줄바꿈 됨
             }
         }
-        
         &_body {
+            width: 100%;
+            height: 84px;
+
             .contents {
                 width: 100%;
                 height: 100%;
 
                 font-size: 14px;
                 color: color.$color-gray-350;
+
+                overflow: hidden;
+                white-space: normal;
+                text-overflow: ellipsis;
+                display: -webkit-box;
+                -webkit-line-clamp: 2;
+                -webkit-box-orient: vertical;
+                word-break: keep-all; // 문단으로 끊어져서 줄바꿈 됨
             }
         }
     }
     &_profile-box {
-        @include mixin.flex-box(center, flex-start);
+        @include mixin.flexbox(center, flex-start);
         width: 100%;
         height: 40px;
 
@@ -134,8 +183,8 @@
                 border-radius: 50%;
             }
         }
-        &__detail {
-            @include mixin.flex-box(flex-start, center);
+        &_detail {
+            @include mixin.flexbox(flex-start, center);
             flex-direction: column;
 
             .name {
@@ -148,6 +197,5 @@
             }
         }
     }
-
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import CardUI from "./components/Card.vue"
+import type { ApiStructure } from "~/types/api";
+import CardComponent from "./components/Card.vue";
+
+const API_KEY="190727eebc064fad996cb098a492dfcf";
+const API_URL=`https://newsapi.org/v2/everything?q=Apple&from=2025-04-21&sortBy=popularity&apiKey=${API_KEY}`;
+const {data, status, error, refresh} = await useAsyncData<ApiStructure>("getNews", ()=> $fetch(API_URL)) //현재 컴포넌트 내에서만 사용할 수 있음. 전역 사용하려면 pinia 필요
+console.log(data.value);
 </script>
 
 <template>
     <div class="page">
-        <CardUI />
-        <CardUI />
-        <CardUI />
-        <CardUI />
-        <CardUI />
-        <CardUI />
+        <CardComponent v-for="article in data?.articles" :key="article.url" :data="article"/>
     </div>
 </template>
 
