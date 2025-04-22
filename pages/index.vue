ecfc815df0ecb5665592f7c521a6091a60b0f29a
Jeonghyun Kim <jh2two@gmail.com>
2025-04-22 14:25:16 +0900

    5_6강_pinia기초

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,29 @@
 <script setup lang="ts">
-import type { ApiStructure } from "~/types/api";
-import CardComponent from "./components/Card.vue";
+import { useStore } from "~/stores/apis";
 
-const API_KEY="190727eebc064fad996cb098a492dfcf";
-const API_URL=`https://newsapi.org/v2/everything?q=Apple&from=2025-04-21&sortBy=popularity&apiKey=${API_KEY}`;
-const {data, status, error, refresh} = await useAsyncData<ApiStructure>("getNews", ()=> $fetch(API_URL)) //현재 컴포넌트 내에서만 사용할 수 있음. 전역 사용하려면 pinia 필요
-console.log(data.value);
+const store = useStore();
+const {count, name, doubleCount, doublePlusOne} = storeToRefs(useStore()); //구조분해할당. 가독성 증가, 반응성이 필요 없는 값을 읽을 때. 구조분해할당한 데이터의 반응성을 유지하려면 storeToRefs 로 감싸면 됨.
+// import type { ApiStructure } from "~/types/api";
+// import CardComponent from "./components/Card.vue";
+
+// const API_KEY="190727eebc064fad996cb098a492dfcf";
+// const API_URL=`https://newsapi.org/v2/everything?q=Apple&from=2025-04-21&sortBy=popularity&apiKey=${API_KEY}`;
+// const {data, status, error, refresh} = await useAsyncData<ApiStructure>("getNews", ()=> $fetch(API_URL)) //현재 컴포넌트 내에서만 사용할 수 있음. 전역 사용하려면 pinia 필요
+// console.log(data.value);
 </script>
 
 <template>
     <div class="page">
-        <CardComponent v-for="article in data?.articles" :key="article.url" :data="article"/>
+        <!-- <CardComponent v-for="article in data?.articles" :key="article.url" :data="article"/> -->
+        {{ name }}
+        <br />
+        {{ count }}
+        <br />
+        {{ doubleCount }}
+        <br />
+        {{ doublePlusOne }}
+        <br />
+        <button @click="store.increment()">증가</button>
     </div>
 </template>
 
---
+++ stores/basic.ts
@@ -0,0 +1,45 @@
+import {defineStore} from "pinia";
+
+export const useStore = defineStore('storeId', () => {
+    // Setup Stores
+    // State
+    const count = ref<number>(10);
+    const name = ref<string>("lemoneeing");
+
+    // Getters
+    const doubleCount = computed(()=>{
+        return count.value * 2;
+    })
+
+    const doublePlusOne = computed(()=>{
+        return count.value * 3;
+    })
+
+    // Actions
+    function increment(){
+        count.value++;
+    }
+
+    function randomizeCounter(){
+        count.value = Math.round(100 * Math.random());
+    }
+
+    return {count, name, doubleCount, doublePlusOne, increment, randomizeCounter};
+
+    // Option Stores
+    // state:() => ({
+    //     count: 10,
+    //     name: "leomoneeing"
+    // }),
+    // getters:{ // store 값을 사용하여 미리 계산한 결과 값 반환. store 값 자체를 변경 X
+    //     doubleCount: (state) => state.count * 2, // 화살표 함수에서는 this 키워드 사용 불가
+    //     doublePlusOne(): number {
+    //         return this.count * 3; // this 키워드를 사용하려면 funciton 함수만 사용 가능
+    //     }
+    // },
+    // actions:{ // store 에 대한 로직 처리. store 값 자체 변경을 위한 것.
+    //     increment(){
+    //         this.count++;
+    //     },
+    // }
+});
